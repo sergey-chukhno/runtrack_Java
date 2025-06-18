9c57da9a8713e46e2404fbb21c4535dcd0c77741
Sergey Chukhno <sergey.chukhno@laplateforme.io>
2025-06-18 06:19:45 +0200

    ✨ Rename Personne.java to job0.java in jour02

---
+++ jour02/job0.java
@@ -0,0 +1,10 @@
+package jour02;
+
+public class Personne {
+  public String nom;
+  public String prenom;
+  protected String dateDeNaissance;
+  protected String lieuDeNaissance;
+  private String adresse;
+  private String telephone; 
+}
